3590e0f346258c13ddbb675c9689e83d9d5d2515
Moustamid <karim1992moustamid@gmail.com>
2021-06-09 12:14:44 -0500

    adding the context api

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import React, { useContext, useEffect, useReducer } from 'react'
-import reducer from '../reducers/products_reducer'
-import { products_url as url } from '../utils/constants'
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import reducer from '../reducers/products_reducer';
+import { products_url as url } from '../utils/constants';
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -11,20 +11,43 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from '../actions';
 
-const initialState = {}
+const initialState = {
+  isSidebarOpen: true,
+};
 
-const ProductsContext = React.createContext()
+const ProductsContext = React.createContext();
 
+//.Provider :
 export const ProductsProvider = ({ children }) => {
+  //.Reducer Hook :
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //. Helper Functions :
+
+  const openSidebar = () => {
+    dispatch({ type: SIDEBAR_OPEN });
+  };
+
+  const closeSidebar = () => {
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
+
   return (
-    <ProductsContext.Provider value='products context'>
+    <ProductsContext.Provider
+      value={{
+        ...state,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
       {children}
     </ProductsContext.Provider>
-  )
-}
-// make sure use
+  );
+};
+
+//- make sure use :
 export const useProductsContext = () => {
-  return useContext(ProductsContext)
-}
+  return useContext(ProductsContext);
+};
